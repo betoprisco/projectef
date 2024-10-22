c4ecff91dc18355e131316b234a71422636a0027
Julio Prisco <julioprisco@icloud.com>
2024-10-22 17:49:49 -0500

    Se hizo la conexión con la db local en SQL Server

--- Program.cs
+++ Program.cs
@@ -1,6 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proyectoef;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddDbContext<TareasContext>(options => options.UseInMemoryDatabase("TareasDB"));
+
+builder.Services.AddSqlServer<TareasContext>(builder.Configuration.GetConnectionString("cnTareas"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapGet("dbConexion", async ([FromServices] TareasContext db) =>
+{
+    await db.Database.EnsureCreatedAsync();
+    return Results.Ok("Base de datos en memoria: " + db.Database.IsInMemory());
+});
 
 app.Run();
