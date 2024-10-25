6a006a6d3fcaafc3b3ecb9e960891087f1b912ce
Julio Prisco <julioprisco@icloud.com>
2024-10-25 15:28:52 -0500

    Obteniendo datos con Entity Framework

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using proyectoef;
+using proyectoef.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +17,12 @@ app.MapGet("dbConexion", async ([FromServices] TareasContext db) =>
 {
     await db.Database.EnsureCreatedAsync();
     return Results.Ok("Base de datos en memoria: " + db.Database.IsInMemory());
+});
+
+app.MapGet("/api/tareas", async ([FromServices] TareasContext dbContext) =>
+{
+    var tareas = await dbContext.Tareas.ToListAsync();
+    return Results.Ok(tareas.Where(t => t.PrioridadTarea == Prioridad.Baja));
 });
 
 app.Run();
