de2c13a2d3c56ca3d5ab51c38072357099bc27de
Julio Prisco <julioprisco@icloud.com>
2024-10-25 12:56:09 -0500

    Creando modelo de tarea con Fluent API

--- TareasContext.cs
+++ TareasContext.cs
@@ -8,4 +8,29 @@ public class TareasContext : DbContext
     public DbSet<Tarea> Tareas { get; set; }
     public DbSet<Categoria> Categorias { get; set; }
     public TareasContext(DbContextOptions<TareasContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Categoria>(categoria =>
+        {
+            categoria.ToTable("Categoria");
+            categoria.HasKey(c => c.CategoriaId);
+            categoria.Property(c => c.Nombre).IsRequired().HasMaxLength(150);
+            categoria.Property(c => c.Descripcion).HasMaxLength(500);
+            
+        });
+
+        modelBuilder.Entity<Tarea>(tarea =>
+        {
+            tarea.ToTable("Tarea");
+            tarea.HasKey(t => t.TareaId);
+            tarea.HasOne(t => t.Categoria).WithMany(c => c.Tareas).HasForeignKey(t => t.CategoriaId);
+            tarea.Property(t => t.Titulo).IsRequired().HasMaxLength(200);
+            tarea.Property(t => t.Descripcion);
+            tarea.Property(t => t.PrioridadTarea).HasConversion<string>();
+            tarea.Property(t => t.FechaCreacion).HasDefaultValueSql("GETDATE()");
+            tarea.Ignore(t => t.Resumen);
+        });
+
+    }
 }
